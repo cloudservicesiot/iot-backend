2ab39319d52e0b68e3b722b2aa690fb5efecd402
DevWhiz1 <ahmadbajwa3095@gmail.com>
2025-11-13 15:59:21 +0500

    update Apis

--- routes/entityHistory.route.js
+++ routes/entityHistory.route.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {getAllEntities,getEntityHistory,getEntityRawHistoryByEntityIdAndDate,getEntityRawHistoryByEntityIdAndDateRange}= require("../controllers/entityHistory.controller")
+const {
+  getAllEntities,
+  getEntityHistory,
+  getEntityRawHistoryByEntityIdAndDate,
+  getEntityRawHistoryByEntityIdAndDateRange,
+  getMatchedEntityValuesForTimestamps
+} = require("../controllers/entityHistory.controller");
 
 router.route('/history').get(getAllEntities);
 router.route('/history/detail/:entityId').get(getEntityHistory);
 router.get("/entity-raw-history-by-date", getEntityRawHistoryByEntityIdAndDate);
 router.get("/entity-raw-history-by-date-range", getEntityRawHistoryByEntityIdAndDateRange);
+router.post("/matched-entity-values", getMatchedEntityValuesForTimestamps);
 
 module.exports = router;
